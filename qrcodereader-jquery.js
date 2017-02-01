f43f1760368ee7a93153b58eb29a61411a82d1a2
Cristian Lorenzetto <cristian.lorenzetto@gmail.com>
2017-02-01 22:16:56 +0100

    init

---
+++ qrcodereader-jquery.js
@@ -0,0 +1,38 @@
+ (function ($) {
+
+   'use strict';
+ var QRCodeReader=window.QRCodeReader;
+ delete window.QRCodeReader;
+ function Plugin(option) {
+    return this.each(function () {
+      var $this   = $(this)
+      var data    = $this.data('qrcodereader')
+      var options = typeof option == 'object' && option;
+      
+
+      if (!data && typeof option == 'string') return
+      if (!data) {
+		  $this.data('qrcodereader', (data = new QRCodeReader(this, options)))
+		  $this.bind('decode',function(){
+			 data.decode(); 
+		  });
+	  }
+      if (typeof option == 'string') data[option]()
+    })
+  }
+
+  var old = $.fn.qrcodeReader
+
+  $.fn.qrcodeReader             = Plugin
+  $.fn.qrcodeReader.Constructor = QRCodeReader
+
+
+  // NO CONFLICT
+  // ===================
+
+  $.fn.qrcodeReader.noConflict = function () {
+    $.fn.qrcodeReader = old
+    return this
+  }
+
+}(jQuery)); 
